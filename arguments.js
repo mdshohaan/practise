d634f1493c9cb964dbea8efe947a80f0d26131d8
mdshohaan <irfanulislamdu@gmail.com>
2025-02-20 16:24:34 +0600

    define object

--- arguments.js
+++ arguments.js
@@ -1,6 +1,18 @@
-class person {
-  constructor(num1,num2) {
-    console.log(num1 + num2);
-  }
+// Defining object
+let person = {
+	first_name: 'Mukul',
+	last_name: 'Latiyan',
+
+	//method
+	getFunction: function () {
+		return (`The name of the person is 
+		${person.first_name} ${person.last_name}`)
+	},
+	//object within object
+	phone_number: {
+		mobile: '12345',
+		landline: '6789'
+	}
 }
-const obj =new person(10,52)
+console.log(person.getFunction());
+console.log(person.phone_number.landline);
