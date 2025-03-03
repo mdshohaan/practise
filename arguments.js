de87bdbe903af3d6b7f4f5c2762e865b49e9e5a4
mdshohaan <irfanulislamdu@gmail.com>
2025-03-03 23:55:36 +0600

    Add DOM

--- arguments.js
+++ arguments.js
@@ -1,18 +1,17 @@
-// Defining object
-let person = {
-	first_name: 'Mukul',
-	last_name: 'Latiyan',
+class Father {
+get(){
+	let num1=10
+	let num2=20
+	console.log(num1 +num2);
 
-	//method
-	getFunction: function () {
-		return (`The name of the person is 
-		${person.first_name} ${person.last_name}`)
-	},
-	//object within object
-	phone_number: {
-		mobile: '12345',
-		landline: '6789'
+}
+}
+class Son{
+	get(){
+		let num1= 100
+		let num2= 200
+		console.log(num1 +num2);
 	}
 }
-console.log(person.getFunction());
-console.log(person.phone_number.landline);
+const obj = new Son()
+console.log(obj.get());
