bc2ad04e5382d5777bb50cd9b31de93273d8e336
mdshohaan <irfanulislamdu@gmail.com>
2025-03-13 05:13:21 +0600

    Add prototype

--- prototype-js/index.js
+++ prototype-js/index.js
@@ -6,7 +6,14 @@ const captain = {
 const player = Object.create(captain); // create an object on the basis of captain
 // console.log(player);
 
-const personMethods = {
+function Person(name, age) {
+  let person = Object.create(Person.prototype);
+  person.name = name; // object.property = value;
+  person.age = age;
+  return Person;
+}
+
+Person.prototype = {
   play() {
     console.log("Person is Playing");
   },
@@ -15,11 +22,5 @@ const personMethods = {
   },
 };
 
-function Person(name, age) {
-  let person = Object.create(personMethods);
-  person.name = name; // object.property = value;
-  person.age = age;
-  return Person;
-}
 const sakib = new Person("Sakib", 35);
 console.log(sakib.name);
