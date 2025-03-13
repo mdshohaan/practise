4c0db1865a4305d4561affb11ea35ca3a8824df0
mdshohaan <irfanulislamdu@gmail.com>
2025-03-13 11:36:42 +0600

    Add new this

--- prototype-js/index.js
+++ prototype-js/index.js
@@ -7,10 +7,11 @@ const player = Object.create(captain); // create an object on the basis of capta
 // console.log(player);
 
 function Person(name, age) {
-  let person = Object.create(Person.prototype);
-  person.name = name; // object.property = value;
-  person.age = age;
-  return Person;
+  // let this = Object.create(Person.prototype);
+  this.name = name; // object.property = value;
+  this.age = age;
+  this.contact = "012352";
+  // return this;
 }
 
 Person.prototype = {
@@ -23,4 +24,5 @@ Person.prototype = {
 };
 
 const sakib = new Person("Sakib", 35);
-console.log(sakib.name);
+console.log(sakib.contact);
+console.log(sakib.play());
