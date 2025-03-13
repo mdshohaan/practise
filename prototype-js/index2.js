e45b24d5d81faa83f4e9fd20abd95b4d832a2b27
mdshohaan <irfanulislamdu@gmail.com>
2025-03-13 15:27:28 +0600

    Add methods objects

--- prototype-js/index2.js
+++ prototype-js/index2.js
@@ -1,3 +1,19 @@
 /** @Title - class
  *  Class is a template code for creating an object
+ * here brandName is object property which is besides this and name is arguments of setBrand
+ * if child and parent have same method ,child method will be used.(its called method overrding)
  */
+class Car {
+  constructor(name) {
+    this.brandName = name;
+    console.log("Creating object");
+  }
+  start() {
+    console.log("start");
+  }
+  stop() {
+    console.log("stop");
+  }
+}
+const bd = new Car();
+bd.setBrand("BMW");
