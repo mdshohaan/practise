519950827ab6bd12f45d48a1cadab709bf18b468
mdshohaan <irfanulislamdu@gmail.com>
2025-03-14 15:57:00 +0600

    Solve the all funcs

---
+++ prototype-js/error-handling.js
@@ -0,0 +1,13 @@
+let a = 5,
+  b = 6;
+console.log(a);
+console.log(b);
+console.log(a + b);
+console.log(a + b);
+try {
+  console.log(a + c);
+} catch (error) {
+  console.log(error);
+}
+
+console.log(a + b);
--- prototype-js/index4.js
+++ prototype-js/index4.js
@@ -1,13 +1,23 @@
 //  Class practise Test
-
+let data = "Sceret Info";
 class User {
   constructor(name, email) {
     this.name = name;
     this.email = email;
   }
   viewData() {
-    console.log(`its ${this.name} and ${this.email}`);
+    console.log("Data :", data);
+  }
+}
+class Admin extends User {
+  constructor(name, email) {
+    super(name, email);
+  }
+  editData() {
+    data = "New Data";
   }
 }
 const student1 = new User("Ajit", "abc@gmail.com");
 const student2 = new User("Arif", "xxx@gmail.com");
+
+const admin = new Admin("Admin", "admin@gmail.com");
