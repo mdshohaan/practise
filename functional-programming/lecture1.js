54b7048e85fcbba43cc0574019186aea0c78a16d
mdshohaan <irfanulislamdu@gmail.com>
2025-03-21 16:55:58 +0600

    Chore:Functional programming js

---
+++ functional-programming/lecture1.js
@@ -0,0 +1,15 @@
+// Declerative
+
+function greeting(name, greet) {
+  return greet + "" + "Irfan";
+}
+const funcs = greeting(name, greet);
+console.log(funcs);
+
+/* Functional Programming =>
+ *  1.Pure Function (1 to 1 function )
+ *  2.Function Composition
+ *  3.Shared State
+ *  4.Mutable Data
+ *  5.Side Effects
+ */
