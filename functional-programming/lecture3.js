3a3879dd10c4ff6d746e3226803e7b5770929659
mdshohaan <irfanulislamdu@gmail.com>
2025-03-22 10:14:13 +0600

    Chore:Inner funcs relate

--- functional-programming/lecture3.js
+++ functional-programming/lecture3.js
@@ -27,3 +27,12 @@ function num(a, b) {
   return sum() + sub() + times() + div();
 }
 const result = num(10, 5); // 72
+
+function outer(x, y) {
+  function inner(a, b) {
+    return a * b + x + y;
+  }
+  return inner(3, 4);
+}
+
+console.log(outer(2, 5));
