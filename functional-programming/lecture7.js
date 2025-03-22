683cd73b788013232d2c030e5ce395f4ba293693
mdshohaan <irfanulislamdu@gmail.com>
2025-03-22 17:04:27 +0600

    Chore:Add return function

--- functional-programming/lecture7.js
+++ functional-programming/lecture7.js
@@ -1,9 +1,24 @@
 // Return A Function From Another Function
 
+// function host() {
+//   console.log("I am Host Function");
+//   return function () {
+//     console.log("I am Inner");
+//   };
+// }
+// here same to same output
 function host() {
   console.log("I am Host Function");
-  return function () {
+  function child() {
     console.log("I am Inner");
-  };
+  }
+  return child;
 }
 host()();
+// Another
+function greetings(msg) {
+  return function (name) {
+    console.log(name + msg);
+  };
+}
+greetings("Hello")("Irfan");
